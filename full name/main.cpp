148c15acd03380902104bb052c97b228977b0bb3
abdalrahmanDifallh <abddf98@gmail.com>
2025-02-06 11:42:53 +0300

    3

---
+++ full name/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+void printFullName(string name1,string name2)
+{
+    cout<<name1+" "+name2;
+}
+int main()
+{
+    printFullName("abd","kk");
+    return 0;
+}
