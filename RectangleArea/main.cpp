0c043bb3cb9d41bf51f169044d09b5db77cd4135
abdalrahmanDifallh <abddf98@gmail.com>
2025-02-11 10:25:11 +0300

    i add calculate area by diangle and side

--- RectangleArea/main.cpp
+++ RectangleArea/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <string>
+#include <cmath>
+
+
+
 
 using namespace std;
 void readNumbers(int &a,int &b)
@@ -8,10 +13,22 @@ void readNumbers(int &a,int &b)
     cout<<"Enter value of B : ";
     cin>>b;
 }
+void readDigonal(int &d)
+{
+    cout<<"Enter value of Diagonal : ";
+    cin>>d;
+
+}
 float calculateRectangleArea(float a,float b)
 {
     return a*b;
 }
+float calculateRectangleAreaByDiagonalAndSide(float a,float d)
+{
+    float x = a*sqrt(pow(d,2)-pow(a,2));
+    return  x;
+}
+
 void printResult(float result)
 {
     cout<<"The rectangle area is : "<<result<<endl;
@@ -21,6 +38,10 @@ void printResult(float result)
     int a,b;
     readNumbers(a,b);
     printResult(calculateRectangleArea(a,b));
+    cout<<"now we will calculate rectangle by diagonal and side , so please enter diagonal "<<endl;
+    int diagonal;
+    readDigonal(diagonal);
+    printResult(calculateRectangleAreaByDiagonalAndSide(a,diagonal));
 
     return 0;
 }
